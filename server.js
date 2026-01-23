8fbf264d4acf09fbf850f7144ce6e03edc4e1c2f
2rd976w26x-hub <2rd976w26x@privaterelay.appleid.com>
2026-01-23 21:23:46 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve everything in the repo as static files
 app.use(express.static(path.join(__dirname)));
 
-// Default route opens the preview page
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "dev", "cardkit_preview.html"));
 });
